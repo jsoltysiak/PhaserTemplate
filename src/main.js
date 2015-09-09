2aa8d69a344149f590044f973de120c1b5df224e
Janusz Sołtysiak <jaso@softwaremind.pl>
2015-09-09 15:10:42 +0200

    Added Phaser game template

---
+++ src/main.js
@@ -0,0 +1,11 @@
+var SpaceHipster = SpaceHipster || {};
+
+SpaceHipster.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '');
+
+SpaceHipster.game.state.add('Boot', SpaceHipster.Boot);
+//uncomment these as we create them through the tutorial
+//SpaceHipster.game.state.add('Preload', SpaceHipster.Preload);
+//SpaceHipster.game.state.add('MainMenu', SpaceHipster.MainMenu);
+//SpaceHipster.game.state.add('Game', SpaceHipster.Game);
+
+SpaceHipster.game.state.start('Boot');
