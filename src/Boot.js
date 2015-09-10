50ffe08e95dad7e3a1bab68c3de2286635923b5d
Janusz Sołtysiak <jaso@softwaremind.pl>
2015-09-10 23:41:25 +0200

    Added some additional setup and example preload assets to the Phaser game template

--- src/Boot.js
+++ src/Boot.js
@@ -4,12 +4,27 @@ GameName.Boot = function () {};
 
 GameName.Boot.prototype = {
 
-    preload: function () {
+    init: function () {
+        this.input.maxPointers = 1;
 
+        this.scale.pageAlignHorizontally = true;
+
+        if (this.game.device.desktop) {
+
+        } else
+        {
+            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            this.scale.setMinMax(480, 260, 1024, 768);
+            this.scale.forceLandscape = true;
+        }
+    },
+
+    preload: function () {
+        this.load.image('preloadBackground', 'assets/images/preload_background.png');
+        this.load.image('preloadBar', 'assets/images/preload_bar.png');
     },
 
     create: function () {
-        this.game.stage.backgroundColor = '#000';
         this.state.start('Preload');
     }
 };
--- src/Preload.js
+++ src/Preload.js
@@ -1,11 +1,21 @@
 var GameName = GameName || {};
 
-GameName.Preload = function () {};
+GameName.Preload = function () {
+    this.background = null;
+    this.preloadBar = null;
+};
 
 GameName.Preload.prototype = {
 
     preload: function () {
+        this.background = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'preloadBackground');
+        this.background.anchor.setTo(0.5);
+        this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY + 100, 'preloadBar');
+        this.preloadBar.anchor.setTo(0.5);
 
+        this.load.setPreloadSprite(this.preloadBar);
+
+        // this.load.image('image', 'assets/images/image.png');
     },
 
     create: function () {
